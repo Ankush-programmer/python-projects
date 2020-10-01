a8d786c1d584372dc8c9c0ca1e71ac063ba2ca0f
Ankush-programmer <72206833+Ankush-programmer@users.noreply.github.com>
2020-10-01 18:50:49 +0530

    Add files via upload

---
+++ boolean.py
@@ -0,0 +1,29 @@
+def cls():
+    import os
+    os.system("cls")
+
+ #function:
+while 1:
+    print("How Many Row You Want To Print")
+    lines= int(input())
+    cls()
+    print("Type 1 Or 0")
+    true_false = int(input())
+    cls()
+    if true_false == 1:
+        for i in range(1,lines+1):
+            for j in range(1,i+1):
+                print("*",end=" ")
+            print()
+    elif true_false == 0:
+        for i in range(lines,0,-1):
+            for j in range(1,i+1):
+                print("*", end="")
+            print()
+    y_n=input("Do you want to try again?(y/n)\n")
+    if y_n=="y":
+        cls()
+        continue
+
+    else:
+        break
